5a07cafe22e96fc44002ae17da323af21596e577
Gayathri Devi <devigayathri640@gmail.com>
2026-06-13 19:57:43 +0530

    Initial commit - Smart Campus Management System

---
+++ DBConnection.java
@@ -0,0 +1,31 @@
+ import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static Connection con = null;
+
+    public static Connection getConnection() {
+
+        try {
+
+            if (con == null || con.isClosed()) {
+
+                Class.forName("com.mysql.cj.jdbc.Driver");
+
+                con = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/smart_campus",
+                        "root",
+                        "Devi@63289");
+
+                System.out.println("Database Connected Successfully");
+            }
+
+
+        } catch (Exception e) {
+            System.out.println("Database Connection Error : " + e.getMessage());
+        }
+
+        return con;
+    }
+}
